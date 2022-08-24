9fe0f44fa4090ba7bb7ad04ce454e0bf0def3fd7
pace11 <ryanjoker87@gmail.com>
2022-08-24 18:59:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from 'reactstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Row>
+        <Col lg={12} md={12}>
+          <Form>
+            <FormGroup>
+              <Label for="exToken">X-App-Token</Label>
+              <Input
+                id="exampleXAppToken"
+                name="xapptoken"
+                type="text"
+                placeholder="isikan X-App-Token ..."
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exEnv">Environtment</Label>
+              <Input
+                id="exampleEnv"
+                name="environtment"
+                type="select"
+              >
+                <option value="uat">Uat</option>
+                <option value="stg">Staging</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="exLeadId">Lead Id</Label>
+              <Input
+                id="exampleLeadId"
+                name="leadid"
+                type="text"
+                placeholder="isikan lead id ..."
+              />
+            </FormGroup>
+            <FormGroup>
+              <Button color="primary">Proses</Button>
+            </FormGroup>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export default App;
+export default App
